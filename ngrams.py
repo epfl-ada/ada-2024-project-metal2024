a1d572e27f234f9ce73224f4ee89f51ea52bbbea
anoush a-p <anoush.azar-pey@epfl.ch>
2024-11-12 22:01:13 +0100

    ngrams

--- ngrams.py
+++ ngrams.py
@@ -1,14 +1,30 @@
 import pandas as pd
 from collections import defaultdict
 from nltk.util import ngrams 
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 import multiprocessing
-from tqdm import tqdm
+from tqdm import tqdm_gui as tqdm # remove _gui if you dont like the GUI and want CLI
 import numpy as np
 import pickle
 
 SUMMARIES_PATH = "MovieSummaries/plot_summaries.txt"
 N = 3 # N as is N-Gram
-RESULTS_PATH = f"results/{N}gram_results.pkl"
+RESULTS_PATH = f"results/morethan100MB/{N}gram_results.pkl"
+
+NLTK_TOKENIZING = False
+if NLTK_TOKENIZING:
+    nltk.download("stopwords")
+    nltk.download("punkt")
+
+CUSTOM_STOPWORDS = [ # words that will be banned from our Ngram
+    ",", ";", ".", "",  
+    "and", "or", "but", "nor", "for", "yet", "the", "a", "an", "of", "in", "to", "on", "with", "by", "at", "from", "about", "as",
+    "he", "she", "it", "they", "we", "I", "you", "is", "are", "was", "were", "be", "have", "has", "her", "his", "had", "do", "does", "will", "would",
+    "that", "which", "what", "when", "who", "whom", "how", "where",
+    "him", "finds","tells", "out", "ends", "up", "so", "can",
+    "none", "other", "than", "named", "their", "then", "while"]
 
 
 def main():
@@ -22,7 +38,7 @@ def main():
         results: list[list] = list(
             tqdm(
                 pool.imap(ngram_computation, summaries_and_ids), 
-                desc="computing ngrams", total=len(summaries_and_ids)
+                desc=f"computing {N}grams", total=len(summaries_and_ids)
             )
         )
 
@@ -30,8 +46,15 @@ def main():
         pickle.dump(results, file)
 
 def tokenize(text: str) -> list[str]:
-    # Basic tokenization (you could use nltk or spaCy for more complex tokenization)
-    return text.lower().split()
+    if NLTK_TOKENIZING:
+        stop_words = set(stopwords.words("english"))
+        tokens = word_tokenize(text.lower())  # Tokenize and lowercase the text
+        filtered_tokens = [word for word in tokens if word.isalpha() and word not in stop_words]
+        return filtered_tokens
+    
+    tokens = text.lower().split()
+    # removing our custom stopwords
+    return [token for token in tokens if token.isalpha() and token not in CUSTOM_STOPWORDS] 
 
 def ngram_computation(summary_and_id: np.ndarray) -> list:
     summary: str = summary_and_id[1]
